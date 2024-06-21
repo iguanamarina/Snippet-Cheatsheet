2ac424627f55ae393681c3023cdbfdceb1150ada
Juan A. Arias <34753691+iguanamarina@users.noreply.github.com>
2024-06-21 12:46:40 +0100

    First commit

---
+++ lista-de-snippets.R
@@ -0,0 +1,90 @@
+# LISTA DE SNIPPETS ACTUALIZADA EL 21/06/2024
+
+# Inicio de Scripts 
+
+snippet preambulo
+    ############################## ################### ############## ### 
+    ##
+    ## Script name: 
+    ##
+    ## Purpose of script: 
+    ## 
+    ##
+    ## Date Created: YYYY-MM-DD
+    ##
+    ## Author: Juan A. Arias (M.Sc.)
+    ## Email: juanantonio.arias.lopez@usc.es
+    ## Webpage: https://juan-arias.xyz
+    ##   
+    ############################## ################### ############## ### 
+
+# Organización de Código
+
+snippet esqueleto
+	####
+	# 1) SECCIÓN 1  ---- 
+	####
+	
+	# Descripción de la sección 1: Explicación de lo que se hará en esta sección.
+	
+	#* Tarea 1 ----
+	
+	# Aquí irá el código para la Tarea 1
+	
+
+	#* Tarea 2 ----
+	
+	# Aquí irá el código para la Tarea 2
+	
+
+	#* Tarea 3 ----
+	
+	# Aquí irá el código para la Tarea 3
+	
+
+	####  
+	# 2) SECCIÓN 2 ----------
+	####
+	
+	# Descripción de la sección 2: Explicación de lo que se hará en esta sección.
+	
+	#* Tarea 1 ----
+	
+	# Aquí irá el código para la Tarea 1
+	
+
+	#* Tarea 2 ----
+	
+	# Aquí irá el código para la Tarea 2
+	
+
+	#* Tarea 3 ----
+	
+	# Aquí irá el código para la Tarea 3
+	
+
+	####
+	# 3) SECCIÓN 3 ----------
+	####
+	
+	# Descripción de la sección 3: Explicación de lo que se hará en esta sección.
+	
+	#* Tarea 1 ----
+	
+	# Aquí irá el código para la Tarea 1
+	
+
+	#* Tarea 2 ----
+	
+	# Aquí irá el código para la Tarea 2
+	
+
+	#* Tarea 3 ----
+	
+	# Aquí irá el código para la Tarea 3
+
+# Estructuras de Control
+
+
+
+# Acciones Rápidas
